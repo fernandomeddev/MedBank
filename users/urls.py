5b651065813a3c4ebbe9372a0d17755e3a32d139
Fernando Medeiros <79485966+fernandomeddev@users.noreply.github.com>
2021-07-20 19:53:08 -0300

    bug correcao

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,16 @@
-from django.urls.conf import path 
+from django.conf.urls import url
+from django.urls import path
 from . import views
 from .views import Historico
 
 app_name = "users"
 
 urlpatterns = [
-        path("account_status/", views.index, name = "account_status"),
-        path("money_transfer/", views.money_transfer, name = "money_transfer"),
         path("report/", Historico.as_view(), name = "report"),
-        path("help/", views.help, name = "help"),
-        path("edit_details/", views.edit_details, name = "edit_details"),
-        path("delete_account/", views.delete_account, name = "delete_account")
+        url(r"^account_status/$", views.index, name = "account_status"),
+        url(r"^money_transfer/", views.money_transfer, name = "money_transfer"),
+        url(r"help/$", views.help, name = "help"),
+        url(r"edit_details/", views.edit_details, name = "edit_details"),
+        url(r"delete_account/$", views.delete_account, name = "delete_account")
+    
     ]
